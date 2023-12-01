fb62a23d98d371298d6528dc24dc39eab772ef05
pritamkumar1995 <pritam17281995@gmail.com>
2023-12-02 04:20:32 +0530

    yarn

--- api.js
+++ api.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const contactRouter = require('./route/route');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(express.json());
 app.use('/',contactRouter);
 
 
