0beeb1accde41864373c2460bc74e9273ad1aeb9
pritamkumar1995 <pritam17281995@gmail.com>
2023-12-02 04:08:40 +0530

    json correction AGAIN

--- api.js
+++ api.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const contactRouter = require('./route/route');
 const cors = require('cors');
 
 const app = express();
-
-
+app.use(bodyParser.json());
 app.use(cors);
 app.use(express.json());
-
-
 app.use('/',contactRouter);
 
 
