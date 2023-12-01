6c65b1d70b0da285113996fb9201ea21af02b26b
pritamkumar1995 <pritam17281995@gmail.com>
2023-12-02 03:51:30 +0530

    json

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const contactRouter = require('./route/route');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
+
+
+app.use(cors);
 app.use(express.json());
+
+
 app.use('/',contactRouter);
 
 
